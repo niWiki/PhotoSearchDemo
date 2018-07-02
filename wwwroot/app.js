46bc48ab9da3f7f3c9617a0a58d7c3560947a80b
Nadya Atanasova <nadya.it@gmail.com>
2018-07-02 20:45:05 +0300

    Create ReactDemo class. ReactDOM calls it initially.

--- wwwroot/app.js
+++ wwwroot/app.js
@@ -1,6 +1,17 @@
 "use strict"
 
+var ReactDemo = React.createClass({
+  render: function () {
+    console.log('render ReactDemo component');
+    return (
+      <div>
+       <h1>Hello React World!</h1>
+      </div>
+    );
+  }
+});
+
 ReactDOM.render(
-  <h1>Hello World</h1>,
+  <ReactDemo />,
   document.getElementById('app')
 );
