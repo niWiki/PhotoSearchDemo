a2fd1e4d61d11002c63bc5b093528ac63647fe08
Nadya Atanasova <nadya.it@gmail.com>
2018-07-02 23:35:56 +0300

    Add button to component and add click event to it.

--- wwwroot/app.js
+++ wwwroot/app.js
@@ -8,6 +8,10 @@ var ReactDemo = React.createClass({
         <TitleBox
           title={this.titleText}
         />
+        <SearchBox
+          text={this.btnText}
+          clickHandler={this.handleBtnClick}
+        />
       </div>
     );
   },
@@ -15,6 +19,10 @@ var ReactDemo = React.createClass({
   // At this point, props and initial state are defined.
   componentWillMount: function () {
     this.titleText = 'Search for imageS';
+    this.btnText = 'Search';
+  },
+  handleBtnClick: function () {
+    alert("clicked");
   },
 });
 
@@ -23,6 +31,21 @@ var TitleBox = React.createClass({
     console.log('render TitleBox component');
     return (
       <h1>{this.props.title}</h1>
+    );
+  }
+});
+
+var SearchBox = React.createClass({
+  render: function () {
+    console.log('render SearchBox component');
+    return (
+      <div>
+        <button
+          onClick={this.props.clickHandler}
+        >
+          {this.props.text}
+        </button>
+      </div>
     );
   }
 });
